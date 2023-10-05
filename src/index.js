0150e95fa78983a6a9f6d5c5dad7baafe0eb7bb7
Manuel Gonzalez <manuel.gonzalez.freelancer@gmail.com>
2023-10-05 14:05:28 +0200

    Primera subida

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+import '@carbon/ibmdotcom-web-components/es/components/dotcom-shell/dotcom-shell-container.js';
+import './index.scss';
+
+window.digitalData = {
+  page: {
+    pageInfo: {
+      language: 'en-US',
+      ibm: {
+        country: 'US',
+        siteID: 'IBMTESTWWW',
+      },
+    },
+    isDataLayerReady: true,
+  },
+};
